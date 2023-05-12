739a9496b3fbdcd402c9c6130883f752b693505f
ifi999 <kkwkjm4231@gmail.com>
2023-05-12 15:35:12 +0900

    feat: Order Entity 추가

---
+++ src/main/java/sample/cafekiosk/spring/domain/order/Order.java
@@ -0,0 +1,54 @@
+package sample.cafekiosk.spring.domain.order;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import sample.cafekiosk.spring.domain.BaseEntity;
+import sample.cafekiosk.spring.domain.orderProduct.OrderProduct;
+import sample.cafekiosk.spring.domain.product.Product;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Entity
+@Table(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    private int totalPrice;
+
+    private LocalDateTime registeredDateTime;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderProduct> orderProducts = new ArrayList<>();
+
+    public Order(List<Product> products, LocalDateTime registerdDateTime) {
+        this.orderStatus = OrderStatus.INIT;
+        this.totalPrice = calculateTotalPrice(products);
+        this.registeredDateTime = registerdDateTime;
+        this.orderProducts = products.stream()
+                .map(p -> new OrderProduct(this, p))
+                .collect(Collectors.toList());
+    }
+
+    private static int calculateTotalPrice(List<Product> products) {
+        return products.stream()
+                .mapToInt(Product::getPrice)
+                .sum();
+    }
+
+    public static Order create(List<Product> products, LocalDateTime registeredDateTime) {
+        return new Order(products, registeredDateTime);
+    }
+}
