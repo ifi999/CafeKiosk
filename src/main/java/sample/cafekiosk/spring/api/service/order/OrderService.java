be971e03b43ddcdd99037ad86c5af7c92f4d5a7e
ifi999 <kkwkjm4231@gmail.com>
2023-05-12 16:23:15 +0900

    fix: 중복 상품 주문 등록할 수 있도록 변경

--- src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -11,6 +11,8 @@ import sample.cafekiosk.spring.domain.product.ProductRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -20,17 +22,24 @@ public class OrderService {
     private final OrderRepository orderRepository;
 
     public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registeredDateTime) {
-
         List<String> productNumbers = request.getProductNumbers();
-        // Product
-        List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
+        List<Product> products = findProductsBy(productNumbers);
 
         Order order = Order.create(products, registeredDateTime);
-
-        // Order
         Order savedOrder = orderRepository.save(order);
 
         return OrderResponse.of(savedOrder);
+    }
+
+    private List<Product> findProductsBy(List<String> productNumbers) {
+        List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
+        Map<String, Product> productMap = products.stream()
+                .collect(Collectors.toMap(product -> product.getProductNumber(), p -> p));
+
+        List<Product> duplicatedProducts = productNumbers.stream()
+                .map(productMap::get)
+                .collect(Collectors.toList());
+        return duplicatedProducts;
     }
 
 }
