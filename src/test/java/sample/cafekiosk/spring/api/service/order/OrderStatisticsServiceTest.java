2315b9da837bd37d9a1503adcf3b50439bb69e7e
ifi999 <kkwkjm4231@gmail.com>
2023-05-14 18:31:04 +0900

    test: 매출 통계 메일 전송 테스트 작성

---
+++ src/test/java/sample/cafekiosk/spring/api/service/order/OrderStatisticsServiceTest.java
@@ -0,0 +1,113 @@
+package sample.cafekiosk.spring.api.service.order;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import sample.cafekiosk.spring.client.mail.MailSendClient;
+import sample.cafekiosk.spring.domain.history.mail.MailSendHistory;
+import sample.cafekiosk.spring.domain.history.mail.MailSendHistoryRepository;
+import sample.cafekiosk.spring.domain.order.Order;
+import sample.cafekiosk.spring.domain.order.OrderRepository;
+import sample.cafekiosk.spring.domain.order.OrderStatus;
+import sample.cafekiosk.spring.domain.orderProduct.OrderProductRepository;
+import sample.cafekiosk.spring.domain.product.Product;
+import sample.cafekiosk.spring.domain.product.ProductRepository;
+import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
+import sample.cafekiosk.spring.domain.product.ProductType;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class OrderStatisticsServiceTest {
+
+    @Autowired
+    private OrderStatisticsService orderStatisticsService;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private OrderProductRepository orderProductRepository;
+
+    @Autowired
+    private MailSendHistoryRepository mailSendHistoryRepository;
+
+    @MockBean
+    private MailSendClient mailSendClient;
+
+    @AfterEach
+    void tearDown() {
+        orderProductRepository.deleteAllInBatch();
+        productRepository.deleteAllInBatch();
+        orderRepository.deleteAllInBatch();
+        mailSendHistoryRepository.deleteAllInBatch();
+    }
+
+    @DisplayName("결제 완료 주문들을 조회하여 매출 통계 메일을 전송한다.")
+    @Test
+    public void sendOrderStatisticsMail() {
+        // given
+        LocalDateTime now = LocalDateTime.of(2023, 5, 14, 0, 0);
+
+        Product product1 = createProduct(ProductType.HANDMADE, "001", 1000);
+        Product product2 = createProduct(ProductType.HANDMADE, "002", 2000);
+        Product product3 = createProduct(ProductType.HANDMADE, "003", 3000);
+        List<Product> products = List.of(product1, product2, product3);
+        productRepository.saveAll(products);
+
+        Order order1 = createPaymentCompletedOrder(LocalDateTime.of(2023, 5, 13, 23, 59, 59), products);
+        Order order2 = createPaymentCompletedOrder(now, products);
+        Order order3 = createPaymentCompletedOrder(LocalDateTime.of(2023, 5, 14, 23, 59, 59), products);
+        Order order4 = createPaymentCompletedOrder(LocalDateTime.of(2023, 5, 15, 0, 0), products);
+
+        // 매번 테스트마다 매번 실제로 메일을 보낼 수 없으므로 Mock 처리
+        // = stubbing
+        Mockito.when(mailSendClient.sendEmail(ArgumentMatchers.any(String.class), ArgumentMatchers.any(String.class), ArgumentMatchers.any(String.class), ArgumentMatchers.any(String.class)))
+                .thenReturn(true);
+
+        // when
+        boolean result = orderStatisticsService.sendOrderStatisticsMail(LocalDate.of(2023, 5, 14), "test@test.com");
+
+        // then
+        assertThat(result).isTrue();
+
+        List<MailSendHistory> histories = mailSendHistoryRepository.findAll();
+        assertThat(histories).hasSize(1)
+                .extracting("content")
+                .contains("총 매출 합계는 12000원입니다.");
+    }
+
+    private Order createPaymentCompletedOrder(LocalDateTime now, List<Product> products) {
+        Order order = Order.builder()
+                .products(products)
+                .orderStatus(OrderStatus.PAYMENT_COMPLETED)
+                .registeredDateTime(now)
+                .build();
+        return orderRepository.save(order);
+    }
+
+    private Product createProduct(ProductType type, String productNumber, int price) {
+        return Product.builder()
+                .type(type)
+                .productNumber(productNumber)
+                .price(price)
+                .sellingStatus(ProductSellingStatus.SELLING)
+                .name("메뉴 이름")
+                .build();
+    }
+
+}
