94fa96edff51109594891175ab341171bbcb97f1
ifi999 <kkwkjm4231@gmail.com>
2023-05-14 16:41:53 +0900

    test: Order Controller 테스트 작성

---
+++ src/test/java/sample/cafekiosk/spring/api/controller/order/OrderControllerTest.java
@@ -0,0 +1,76 @@
+package sample.cafekiosk.spring.api.controller.order;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
+import sample.cafekiosk.spring.api.service.order.OrderService;
+
+import java.util.List;
+
+@WebMvcTest(OrderController.class)
+class OrderControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private OrderService orderService;
+
+    @DisplayName("신규 주문을 등록한다.")
+    @Test
+    public void createOrder() throws Exception {
+        // given
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .productNumbers(List.of("001"))
+                .build();
+
+        // when
+
+        // then
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/v1/orders/new")
+                        .content(objectMapper.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("200"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("OK"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("OK"));
+    }
+
+    @DisplayName("신규 주문을 등록할 시에 상품 번호는 1개 이상이어야 한다.")
+    @Test
+    public void createOrderWithOutProductNumbers() throws Exception {
+        // given
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .build();
+
+        // when
+
+        // then
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/api/v1/orders/new")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("400"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("BAD_REQUEST"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("상품 번호 리스트는 필수입니다."))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data").isEmpty());
+    }
+
+}
