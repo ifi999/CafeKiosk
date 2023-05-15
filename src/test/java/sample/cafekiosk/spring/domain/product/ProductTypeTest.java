a0cb7430e97bacc2b9b8655df97d1ff8349b8ef5
ifi999 <kkwkjm4231@gmail.com>
2023-05-15 17:40:16 +0900

    test: Enum 여러개 체크 -> @ParameterizedTest 로 수정

--- src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
@@ -3,36 +3,49 @@ package sample.cafekiosk.spring.domain.product;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductTypeTest {
 
-    @DisplayName("상품 타입이 재고 관련 타입인지 체크한다.")
-    @Test
-    public void containsStockType() {
-        // given
-        ProductType givenType = ProductType.HANDMADE;
+//    @DisplayName("상품 타입이 재고 관련 타입인지 체크한다.")
+//    @Test
+//    public void containsStockType() {
+//        // given
+//        ProductType givenType = ProductType.HANDMADE;
+//
+//        // when
+//        boolean result = ProductType.containsStockType(givenType);
+//
+//        // then
+//        assertThat(result).isFalse();
+//    }
+//
+//    @DisplayName("상품 타입이 재고 관련 타입인지 체크한다.")
+//    @Test
+//    public void containsStockType2() {
+//        // given
+//        ProductType givenType = ProductType.BAKERY;
+//
+//        // when
+//        boolean result = ProductType.containsStockType(givenType);
+//
+//        // then
+//        assertThat(result).isTrue();
+//    }
 
+    @CsvSource({"HANDMADE, false", "BOTTLE, true", "BAKERY, true"})
+    @DisplayName("상품 타입이 재고 관련 타입인지 체크한다.")
+    @ParameterizedTest
+    public void containsStockType(ProductType productType, boolean expected) {
         // when
-        boolean result = ProductType.containsStockType(givenType);
+        boolean result = ProductType.containsStockType(productType);
 
         // then
-        assertThat(result).isFalse();
-    }
-
-    @DisplayName("상품 타입이 재고 관련 타입인지 체크한다.")
-    @Test
-    public void containsStockType2() {
-        // given
-        ProductType givenType = ProductType.BAKERY;
-
-        // when
-        boolean result = ProductType.containsStockType(givenType);
-
-        // then
-        assertThat(result).isTrue();
+        assertThat(result).isEqualTo(expected);
     }
 
 }
